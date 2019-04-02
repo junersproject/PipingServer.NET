0533c5dba624dcbd1eb4f24b30ae7cc95f75dbe7
juner <juner@juner.net>
2019-04-02 18:10:34 +0900

    first commit

---
+++ PipingServer.NET/IService.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace Piping
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebInvoke(UriTemplate = "{perKey}")]
+        Stream Pipe(string perKey, Stream InputStream);
+    }
+}
---
+++ PipingServer.NET/Service.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Piping
+{
+    public class Service : IService
+    {
+        public Stream Pipe(string per, Stream inputStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

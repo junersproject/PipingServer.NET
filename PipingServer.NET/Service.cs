bb6740996fb7cb8d621b3c00ec41221033a0f5b1
juner <juner@juner.net>
2019-04-18 21:01:41 +0900

    画面の追加

--- PipingServer.NET/Service.cs
+++ PipingServer.NET/Service.cs
@@ -1,10 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Net;
 using System.IO;
+using System.Reflection;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
 using System.ServiceModel.Description;
 using System.ServiceModel.Web;
+using System.Text;
+using System.Web;
+using System.Security;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Piping
 {
@@ -13,6 +19,8 @@ namespace Piping
     [ServiceContract(SessionMode= SessionMode.NotAllowed)]
     public class Service : IService
     {
+        string BasePath;
+        Lazy<FileVersionInfo> VERSION = new Lazy<FileVersionInfo>(() => FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location));
         /// <summary>
         /// デフォルト設定の反映
         /// </summary>
@@ -47,6 +55,20 @@ namespace Piping
                 MaxReceivedMessageSize = MaxReceivedMessageSize,
             }, "").EndpointBehaviors.Add(new WebHttpBehavior());
         }
+        public Service()
+        {
+            BasePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            NAME_TO_RESERVED_PATH = new Dictionary<string, Func<OutgoingWebResponseContext, Stream>>
+            {
+                {"/", DefaultPage },
+                {"/version",  VersionPage},
+                {"/help", HelpPage },
+                {"/favicon.ico", FileGetGenerator("/favicon.ico") },
+                {"/robots.txt", FileGetGenerator("/robots.txt") },
+            };
+        }
+        private Encoding Encoding = new UTF8Encoding(false);
+        private Dictionary<string, Func<OutgoingWebResponseContext, Stream>> NAME_TO_RESERVED_PATH;
         /// <summary>
         /// エントリーポイント
         /// </summary>
@@ -55,13 +77,138 @@ namespace Piping
         [OperationBehavior(ReleaseInstanceMode = ReleaseInstanceMode.None)]
         public Stream Default(Stream inputStream)
         {
-            var IncomingRequest = WebOperationContext.Current.IncomingRequest;
-            var Method = IncomingRequest.Method;
-            var RequestUri = IncomingRequest.UriTemplateMatch.RequestUri;
+            var Current = WebOperationContext.Current;
+            var Request = Current.IncomingRequest;
+            var Response = Current.OutgoingResponse;
+            var Method = Request.Method;
+            var RequestUri = Request.UriTemplateMatch.RequestUri;
             var reqPath = RequestUri.LocalPath.TrimEnd('/');
-
+            if (reqPath == string.Empty)
+                reqPath = "/";
+            switch (Method)
+            {
+                case "POST":
+                case "PUT":
+                    if (NAME_TO_RESERVED_PATH.TryGetValue(reqPath, out _))
+                    {
+                        Response.StatusCode = HttpStatusCode.BadRequest;
+                        var Encoding = Response.BindingWriteEncoding;
+                        var Bytes = Encoding.GetBytes($"[ERROR] Cannot send to a reserved path '{reqPath}'. (e.g. '/mypath123')\n");
+                        Response.ContentLength = Bytes.Length;
+                        Response.ContentType = $"text/plain;charset={Encoding.WebName}";
+                        return new MemoryStream(Bytes);
+                    }
+                    throw new NotImplementedException();
+                case "GET":
+                    if (NAME_TO_RESERVED_PATH.TryGetValue(reqPath, out var Generator))
+                        return Generator(Response);
+                    throw new NotImplementedException();
+                case "OPTIONS":
+                    return Options(Response);
+                default:
+                    return NotImplemented(Response);
+            }
             throw new NotImplementedException();
         }
-        internal Stream 
+        protected Stream DefaultPage(OutgoingWebResponseContext Response)
+        {
+            var Encoding = Response.BindingWriteEncoding;
+            var Bytes = Encoding.GetBytes(Properties.Resource.DefaultPage);
+            Response.ContentLength = Bytes.Length;
+            Response.ContentType = $"text/html;charset={Encoding.WebName}";
+            return new MemoryStream(Bytes);
+        }
+        protected Stream HelpPage(OutgoingWebResponseContext Response)
+        {
+            var url = HttpContext.Current.Server.MapPath(".");
+            var VERSION = this.VERSION.Value;
+            var Encoding = Response.BindingWriteEncoding;
+            var Bytes = Encoding.GetBytes(@$"Help for piping - server { VERSION}
+(Repository: https://github.com/nwtgck/piping-server)
+
+======= Get  =======
+curl ${url}/mypath
+
+======= Send =======
+# Send a file
+curl -T myfile ${url}/mypath
+
+# Send a text
+echo 'hello!' | curl -T - ${url}/mypath
+
+# Send a directory (zip)
+zip -q -r - ./mydir | curl -T - ${url}/mypath
+
+# Send a directory (tar.gz)
+tar zfcp - ./mydir | curl -T - ${url}/mypath
+
+# Encryption
+## Send
+cat myfile | openssl aes-256-cbc | curl -T - ${url}/mypath
+## Get
+curl ${url}/mypath | openssl aes-256-cbc -d");
+            Response.ContentLength = Bytes.Length;
+            Response.ContentType = $"text/plain;charset={Encoding.WebName}";
+            return new MemoryStream(Bytes);
+        }
+        protected Stream VersionPage(OutgoingWebResponseContext Response)
+        {
+            var VERSION = this.VERSION.Value;
+            var Encoding = Response.BindingWriteEncoding;
+            var Bytes = Encoding.GetBytes($"{VERSION}\n");
+            Response.ContentLength = Bytes.Length;
+            Response.ContentType = $"text/plain;charset={Encoding.WebName}";
+            return new MemoryStream(Bytes);
+        }
+        protected Stream Options(OutgoingWebResponseContext Response)
+        {
+            Response.StatusCode = HttpStatusCode.OK;
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            Response.Headers.Add("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, OPTIONS");
+            Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Content-Disposition");
+            Response.Headers.Add("Access-Control-Max-Age", "86400");
+            Response.ContentLength = 0;
+            return new MemoryStream(new byte[0]);
+        }
+        protected Stream NotImplemented(OutgoingWebResponseContext Response)
+        {
+            Response.StatusCode = HttpStatusCode.NotImplemented;
+            Response.ContentLength = 0;
+            return new MemoryStream(new byte[0]);
+        }
+        protected bool ExistsFile(string FileName)
+        {
+            var FilePath = Path.Combine(BasePath, FileName.TrimStart('/'));
+            return File.Exists(FilePath);
+        }
+        protected Func<OutgoingWebResponseContext, Stream> FileGetGenerator(string FileName)
+        {
+            var FilePath = Path.Combine(BasePath, FileName.TrimStart('/'));
+            return (Response) =>
+            {
+                try
+                {
+                    var Bytes = File.ReadAllBytes(FilePath);
+                    Response.StatusCode = HttpStatusCode.OK;
+                    Response.ContentType = MimeMapping.GetMimeMapping(FilePath);
+                    Response.ContentLength = Bytes.Length;
+                    return new MemoryStream(Bytes);
+                }
+                catch (FileNotFoundException)
+                {
+                    Response.StatusCode = HttpStatusCode.NotFound;
+                }
+                catch (SecurityException)
+                {
+                    Response.StatusCode = HttpStatusCode.NotFound;
+                }
+                catch (Exception)
+                {
+                    Response.StatusCode = HttpStatusCode.InternalServerError;
+                }
+                Response.ContentLength = 0;
+                return new MemoryStream(new byte[0]);
+            };
+        }
     }
 }

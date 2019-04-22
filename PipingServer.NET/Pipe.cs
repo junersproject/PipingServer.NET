a51c2949ab975241cfffcefe0df8d020d3239ad0
juner <juner@juner.net>
2019-04-22 21:40:14 +0900

    戻り値 Stream を Task<Stream>に変更

---
+++ PipingServer.NET/Pipe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Piping
+{
+    public class Pipe
+    {
+        public ReqRes Sender { get; }
+        public IReadOnlyCollection<ReqRes> Receivers { get; }
+        public Pipe(ReqRes Sender, IList<ReqRes> Receivers)
+            => (this.Sender, this.Receivers) = (Sender, new ReadOnlyCollection<ReqRes>(Receivers));
+        public Pipe(ReqRes Sender, IEnumerable<ReqRes> Receivers) : this(Sender, Receivers.ToList()) { }
+    }
+}
--- PipingServer.NET/UnestablishedPipe.cs
+++ PipingServer.NET/UnestablishedPipe.cs
@@ -5,7 +5,7 @@ namespace Piping
     public class UnestablishedPipe
     {
         public ReqResAndUnsubscribe Sender { get; set; }
-        public List<ReqResAndUnsubscribe> Receivers { get; } = new List<ReqResAndUnsubscribe>();
+        public IList<ReqResAndUnsubscribe> Receivers { get; } = new List<ReqResAndUnsubscribe>();
         public int ReceiversCount { get; set; }
     }
 }

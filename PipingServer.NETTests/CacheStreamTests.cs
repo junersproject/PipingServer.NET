c49c3c8115ed233eaf7dd0aada707f37c5f04c61
juner <juner@juner.net>
2019-04-28 13:46:23 +0900

    CacheStream のテストが 内容を見るものではなかったので中を見る様に修正。
    なお、現状エラーとなる。
    CacheStream からの読み込みに問題があるのだろうか？

--- PipingServer.NETTests/CacheStreamTests.cs
+++ PipingServer.NETTests/CacheStreamTests.cs
@@ -17,70 +17,81 @@ namespace Piping.Tests
         public void CacheStreamSyncTest()
         {
             var Encoding = new UTF8Encoding(false);
+            var Data = Enumerable.Range(0, 5).Select(v => $"number: {v}").ToArray();
             using (var TokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(1)))
             using (var Base = new MemoryStream())
             using (var Cache = new CacheStream(Base))
             {
                 var Token = TokenSource.Token;
                 using (var writer = new StreamWriter(Base, Encoding, 1024, true))
-                    foreach (var number in Enumerable.Range(0, 5))
+                    foreach (var Text in Data)
                     {
                         Token.ThrowIfCancellationRequested();
-                        var Text = $"number:{number}";
                         writer.WriteLine(Text);
                         Trace.WriteLine($"write: {Text}");
                     }
-                using (var reader = new StreamReader(Cache, Encoding, false, 1024, true))
-                {
-                    Token.ThrowIfCancellationRequested();
-                    var Text = reader.ReadLine();
-                    Trace.WriteLine($"cache read: {Text}");
-                }
-                foreach (var (os, index) in Cache.OutputStreams.Select((v, i) => (v, i)))
-                    using (var reader = new StreamReader(os, Encoding, false, 1024, true))
+                using (var reader = new StreamReader(Cache, Encoding, false, 1024, false))
+                    foreach (var ExpectText in Data)
+
                     {
                         Token.ThrowIfCancellationRequested();
-                        var Text = reader.ReadToEnd();
-                        Trace.WriteLine($"cache {index} read: {Text}");
+                        var Text = reader.ReadLine();
+                        Trace.WriteLine($"cache read: {Text}");
+                        Assert.AreEqual(ExpectText, Text);
                     }
+                foreach (var (os, index) in Cache.OutputStreams.Select((v, i) => (v, i)))
+                    using (var reader = new StreamReader(os, Encoding, false, 1024, true))
+                        foreach(var ExpectText in Data)
+                        {
+
+                            Token.ThrowIfCancellationRequested();
+                            var Text = reader.ReadLine();
+                            Trace.WriteLine($"cache {index} read: {Text}");
+                            Assert.AreEqual(ExpectText, Text);
+                        }
             }
         }
         [TestMethod, TestCategory("ShortTime")]
         public async Task CacheStreamAsyncSyncTest()
         {
             var Encoding = new UTF8Encoding(false);
+            var Data = Enumerable.Range(0, 5).Select(v => $"number: {v}").ToArray();
             using (var TokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(1)))
             using (var Base = new MemoryStream())
             using (var Cache = new CacheStream(Base))
             {
                 var Token = TokenSource.Token;
                 using (var writer = new StreamWriter(Base, Encoding, 1024, true))
-                    foreach (var number in Enumerable.Range(0, 5))
+                    foreach (var Text in Data)
                     {
                         Token.ThrowIfCancellationRequested();
-                        var Text = $"number:{number}";
                         await writer.WriteLineAsync(Text);
                         Trace.WriteLine($"write: {Text}");
                     }
-                using (var reader = new StreamReader(Cache, Encoding, false, 1024, true))
-                {
-                    Token.ThrowIfCancellationRequested();
-                    var Text = await reader.ReadLineAsync();
-                    Trace.WriteLine($"cache read: {Text}");
-                }
-                foreach (var (os, index) in Cache.OutputStreams.Select((v, i) => (v, i)))
-                    using (var reader = new StreamReader(os, Encoding, false, 1024, true))
+                using (var reader = new StreamReader(Cache, Encoding, false, 1024, false))
+                    foreach (var ExpectText in Data)
                     {
                         Token.ThrowIfCancellationRequested();
-                        var Text = await reader.ReadToEndAsync();
-                        Trace.WriteLine($"cache {index} read: {Text}");
+                        var Text = await reader.ReadLineAsync();
+                        Trace.WriteLine($"cache read: {Text}");
+                        Assert.AreEqual(ExpectText, Text);
                     }
+                foreach (var (os, index) in Cache.OutputStreams.Select((v, i) => (v, i)))
+                    using (var reader = new StreamReader(os, Encoding, false, 1024, true))
+                        foreach (var ExpectText in Data)
+                        {
+                            Token.ThrowIfCancellationRequested();
+                            var Text = await reader.ReadLineAsync();
+                            Trace.WriteLine($"cache {index} read: {Text}");
+                            Assert.AreEqual(ExpectText, Text);
+                        }
             }
         }
         [TestMethod, TestCategory("ShortTime")]
         public async Task CacheStreamAsyncTest()
         {
             var Encoding = new UTF8Encoding(false);
+            var Data = Enumerable.Range(0, 5).Select(v => $"number: {v}").ToArray();
             using (var TokenSource = new CancellationTokenSource(TimeSpan.FromMinutes(1)))
             using (var Base = new MemoryStream())
             using (var Cache = new CacheStream(Base))
@@ -89,32 +100,35 @@ namespace Piping.Tests
                 var writerTask = Task.Run(async () =>
                 {
                     using (var writer = new StreamWriter(Base, Encoding, 1024, true))
-                        foreach (var number in Enumerable.Range(0, 5))
+                        foreach (var Text in Data)
                         {
                             Token.ThrowIfCancellationRequested();
-                            var Text = $"number:{number}";
                             await writer.WriteLineAsync(Text);
                             Trace.WriteLine($"write: {Text}");
                         }
                 });
                 var readerTask = Task.Run(async () =>
                 {
-                    using (var reader = new StreamReader(Cache, Encoding, false, 1024, true))
-                    {
-                        Token.ThrowIfCancellationRequested();
-                        var Text = await reader.ReadLineAsync();
-                        Trace.WriteLine($"cache read: {Text}");
-                    }
+                    using (var reader = new StreamReader(Cache, Encoding, false, 1024, false))
+                        foreach (var ExpectText in Data)
+                        {
+                            Token.ThrowIfCancellationRequested();
+                            var Text = await reader.ReadLineAsync();
+                            Trace.WriteLine($"cache read: {Text}");
+                            Assert.AreEqual(ExpectText, Text);
+                        }
                 });
                 var cacheReaderTask = Task.Run(async () =>
                 {
                     foreach (var (os, index) in Cache.OutputStreams.Select((v, i) => (v, i)))
                         using (var reader = new StreamReader(os, Encoding, false, 1024, true))
-                        {
-                            Token.ThrowIfCancellationRequested();
-                            var Text = await reader.ReadToEndAsync();
-                            Trace.WriteLine($"cache {index} read: {Text}");
-                        }
+                            foreach (var ExpectText in Data)
+                            {
+                                Token.ThrowIfCancellationRequested();
+                                var Text = await reader.ReadLineAsync();
+                                Trace.WriteLine($"cache {index} read: {Text}");
+                                Assert.AreEqual(ExpectText, Text);
+                            }
                 });
                 await Task.WhenAll(writerTask, readerTask, cacheReaderTask);
             }

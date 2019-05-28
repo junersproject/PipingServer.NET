a052655dcab0ae56dcd28b976aadc5c7d8739438
juner <juner@juner.net>
2019-05-29 08:53:20 +0900

    Console の SelfHost版を追加

--- PipingServer.NET/Properties/AssemblyInfo.cs
+++ PipingServer.NET/Properties/AssemblyInfo.cs
@@ -37,3 +37,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: InternalsVisibleTo("PipingServer.NETTests")]
+[assembly: InternalsVisibleTo("PipingServerConsole")]
---
+++ PipingServerConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piping.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var Source = new TaskCompletionSource<bool>();
+            System.Console.CancelKeyPress += (o, arg) =>
+            {
+                arg.Cancel = false;
+                Source.TrySetResult(true);
+            };
+            var BaseAddress = new Uri("http://localhost/Console");
+            using (var Self = new SelfHost())
+            {
+                Self.Open(BaseAddress);
+                System.Console.WriteLine("Service Console Start.");
+                try
+                {
+                    await Source.Task;
+                }
+                finally
+                {
+                    System.Console.WriteLine("Service Console Stop.");
+                }
+            }
+        }
+    }
+}

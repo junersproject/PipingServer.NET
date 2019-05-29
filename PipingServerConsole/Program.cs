dcae91bce070d3bc5766841fd1a3abd5bda847fa
juner <juner@juner.net>
2019-05-29 11:23:55 +0900

    Consoleの修正
    * 画面が閉じれなかったのの修正
    * URLの色を変更
    * エラー時の色を変更

--- PipingServerConsole/Program.cs
+++ PipingServerConsole/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Piping.Console
@@ -13,11 +10,15 @@ namespace Piping.Console
             var Source = new TaskCompletionSource<bool>();
             System.Console.CancelKeyPress += (o, arg) =>
             {
-                arg.Cancel = false;
+                arg.Cancel = Source.Task.IsCompleted;
                 Source.TrySetResult(true);
             };
             var BaseAddress = new Uri("http://localhost/Console");
-            using (var Self = new SelfHost())
+            using var Self = new SelfHost();
+            System.Console.Write($"Service Open: ");
+            using (SetColor(ConsoleColor.Blue, ConsoleColor.Yellow))
+                System.Console.WriteLine($"{BaseAddress}");
+            try
             {
                 Self.Open(BaseAddress);
                 System.Console.WriteLine("Service Console Start.");
@@ -29,6 +30,43 @@ namespace Piping.Console
                 {
                     System.Console.WriteLine("Service Console Stop.");
                 }
+            }
+            catch (Exception e)
+            {
+                using var s = SetColor(ConsoleColor.White, ConsoleColor.Red);
+                System.Console.WriteLine(e);
+                throw;
+            }
+        }
+        /// <summary>
+        /// コンソールの文字色・背景色を設定する
+        /// </summary>
+        /// <param name="Foreground"></param>
+        /// <param name="Background"></param>
+        /// <returns></returns>
+        private static IDisposable SetColor(ConsoleColor? Foreground = null, ConsoleColor? Background = null)
+        { 
+            var _Forground = Foreground is ConsoleColor ? System.Console.ForegroundColor : (ConsoleColor?)null;
+            var _Background = Background is ConsoleColor ? System.Console.BackgroundColor : (ConsoleColor?)null;
+            try
+            {
+                if (Foreground is ConsoleColor forground)
+                    System.Console.ForegroundColor = forground;
+                if (Background is ConsoleColor background)
+                    System.Console.BackgroundColor = background;
+            }
+            catch
+            {
+                SetBack();
+                throw;
+            }
+            return Disposable.Create(SetBack);
+            void SetBack()
+            {
+                if (_Forground is ConsoleColor forground)
+                    System.Console.ForegroundColor = forground;
+                if (_Background is ConsoleColor background)
+                    System.Console.BackgroundColor = background;
             }
         }
     }

91a88740b3f70c6a338d6ba381f3e645faacf0c5
juner <juner@juner.net>
2019-06-27 02:43:24 +0900

    CompletableQueueStream の完成

--- PipingServer.NET/Streams/CompletableQueueStream.cs
+++ PipingServer.NET/Streams/CompletableQueueStream.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.IO;
 using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Piping.Streams
@@ -13,22 +14,22 @@ namespace Piping.Streams
     {
         readonly bool isWritableMode = true;
         public static CompletableQueueStream Empty { get; } = new CompletableQueueStream(false);
-        readonly BlockingCollection<byte[]> data;
-        byte[] _currentBlock = Array.Empty<byte>();
-        public int BoundedCapacity => data.BoundedCapacity;
+        readonly Channel<ReadOnlyMemory<byte>> data;
+        ReadOnlyMemory<byte> _currentBlock = ReadOnlyMemory<byte>.Empty;
         int _currentBlockIndex = 0;
-        public int BufferedWrites => data.Count;
-        public bool IsAddingCompleted => data.IsAddingCompleted;
-        public bool IsCompleted => data.IsCompleted;
-        public void CompleteAdding() => data.CompleteAdding();
-        public CompletableQueueStream() => data = new BlockingCollection<byte[]>();
-        public CompletableQueueStream(int boundedCapacity) => data = new BlockingCollection<byte[]>(boundedCapacity);
+        public bool IsAddingCompleted { get; private set; } = false;
+        public void CompleteAdding(){
+            data.Writer.TryComplete();
+            IsAddingCompleted = true;
+        }
+        public CompletableQueueStream() => data = Channel.CreateUnbounded<ReadOnlyMemory<byte>>();
+        public CompletableQueueStream(int boundedCapacity) => data = Channel.CreateBounded<ReadOnlyMemory<byte>>(boundedCapacity);
         private CompletableQueueStream(bool isWritableMode) : this(1) => this.isWritableMode = isWritableMode;
         public override bool CanRead => isWritableMode;
 
         public override bool CanSeek => false;
 
-        public override bool CanWrite => isWritableMode && !data.IsAddingCompleted;
+        public override bool CanWrite => isWritableMode && !IsAddingCompleted;
         public override long Length => throw new NotSupportedException();
 
         public override long Position { get => throw new NotSupportedException(); set => throw new NotImplementedException(); }
@@ -38,38 +39,46 @@ namespace Piping.Streams
             //noop
         }
 
-        public override int Read(byte[] buffer, int offset, int count)
+        public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken Token = default)
         {
             if (!CanRead)
                 throw new InvalidOperationException();
-            if (_currentBlock.Length == 0 || _currentBlockIndex == _currentBlock.Length)
-                if (!GetNextBlock())
+            if (_currentBlock.IsEmpty || _currentBlockIndex == _currentBlock.Length)
+                if (!await GetNextBlockAsync(Token).ConfigureAwait(false))
                     return 0;
-            int minCount = Math.Min(count, _currentBlock.Length - _currentBlockIndex);
-            Array.Copy(_currentBlock, _currentBlockIndex, buffer, offset, minCount);
+            var minCount = Math.Min(buffer.Length, _currentBlock.Length - _currentBlockIndex);
+            _currentBlock.Slice(_currentBlockIndex, minCount).CopyTo(buffer);
             _currentBlockIndex += minCount;
             return minCount;
         }
-
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken Token)
+            => await ReadAsync(buffer.AsMemory().Slice(offset, count), Token).ConfigureAwait(false);
+        public override int Read(Span<byte> buffer)
+        {
+            if (!CanRead)
+                throw new InvalidOperationException();
+            if (_currentBlock.IsEmpty || _currentBlockIndex == _currentBlock.Length)
+                if (!GetNextBlockAsync().Result)
+                    return 0;
+            var minCount = Math.Min(buffer.Length, _currentBlock.Length - _currentBlockIndex);
+            _currentBlock.Span.Slice(_currentBlockIndex, minCount).CopyTo(buffer);
+            _currentBlockIndex += minCount;
+            return minCount;
+        }
+        public override int Read(byte[] buffer, int offset, int count) => Read(buffer.AsSpan().Slice(offset, count));
         /// <summary>
         /// Loads the next block in to <see cref="_currentBlock"/>
         /// </summary>
         /// <returns>True if the next block was retrieved.</returns>
-        private bool GetNextBlock(CancellationToken Token = default)
+        private async ValueTask<bool> GetNextBlockAsync(CancellationToken Token = default)
         {
-            if (!data.TryTake(out _currentBlock, Timeout.Infinite, Token))
+            if (await data.Reader.WaitToReadAsync(Token))
             {
-                if (data.IsCompleted)
+                if (!data.Reader.TryRead(out _currentBlock))
                     return false;
-                try
-                {
-                    _currentBlock = data.Take(Token);
-                }
-                catch (InvalidOperationException)
-                {
-                    return false;
-                }
             }
+            else
+                return false;
             _currentBlockIndex = 0;
             return true;
         }
@@ -77,18 +86,34 @@ namespace Piping.Streams
         public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
 
         public override void SetLength(long value) => throw new NotSupportedException();
-
-        public override void Write(byte[] buffer, int offset, int count)
+        public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+        { 
+            if (!CanWrite)
+                throw new InvalidOperationException();
+            var localArray = new byte[buffer.Length];
+            if (await data.Writer.WaitToWriteAsync(cancellationToken).ConfigureAwait(false))
+            {
+                buffer.CopyTo(localArray);
+                data.Writer.TryWrite(localArray);
+            }
+        }
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+            => await WriteAsync(buffer.AsMemory().Slice(offset, count), cancellationToken).ConfigureAwait(false);
+        public override void Write(ReadOnlySpan<byte> buffer)
         {
             if (!CanWrite)
                 throw new InvalidOperationException();
-            var localArray = new byte[count];
-            Array.Copy(buffer, offset, localArray, 0, count);
-            data.Add(localArray);
+            var localArray = new byte[buffer.Length];
+            if (data.Writer.WaitToWriteAsync().Result)
+            {
+                buffer.CopyTo(localArray);
+                data.Writer.TryWrite(localArray);
+            }
         }
+        public override void Write(byte[] buffer, int offset, int count) => Write(buffer.AsSpan().Slice(offset, count));
         protected override void Dispose(bool disposing)
         {
-            data.Dispose();
+            data.Writer.TryComplete(new ObjectDisposedException("Disposed"));
             base.Dispose(disposing);
         }
     }
--- PipingServer.NETTests/Streams/CompletableQueueStreamTests.cs
+++ PipingServer.NETTests/Streams/CompletableQueueStreamTests.cs
@@ -32,14 +32,14 @@ namespace Piping.Streams.Tests
             using (var TokenSource = new CancellationTokenSource(Time))
                 Task.WaitAny(new[] { Task.Run(() => ReadBytes = stream.Read(buffer, 0, buffer.Length)) }, TokenSource.Token);
             Assert.AreEqual(buffer.Length, ReadBytes);
-            ReadBytes = stream.Read(buffer, 0, data.Length);
+            ReadBytes = stream.Read(buffer, 0, buffer.Length);
             Assert.AreEqual(buffer.Length, ReadBytes);
             using (var TokenSource = new CancellationTokenSource(Time))
                 Assert.ThrowsException<OperationCanceledException>(
                 () => Task.WaitAny(new[] { Task.Run(() => stream.Read(buffer, 0, buffer.Length)) }, TokenSource.Token));
             stream.Write(data, 0, data.Length);
             Assert.AreEqual(buffer.Length, ReadBytes);
-            ReadBytes = stream.Read(buffer, 0, data.Length);
+            ReadBytes = stream.Read(buffer, 0, buffer.Length);
             Assert.AreEqual(buffer.Length, ReadBytes);
         }
         [TestMethod, TestCategory("ShortTime")]
@@ -70,9 +70,9 @@ namespace Piping.Streams.Tests
             using (var TokenSource = new CancellationTokenSource(Time))
                 Assert.ThrowsException<OperationCanceledException>(
                 () => Task.WaitAny(new[] { Task.Run(() => stream.Read(buffer)) }, TokenSource.Token));
-            stream.Write(data, 0, data.Length);
+            stream.Write(data);
             Assert.AreEqual(buffer.Length, ReadBytes);
-            ReadBytes = stream.Read(buffer, 0, data.Length);
+            ReadBytes = stream.Read(buffer);
             Assert.AreEqual(buffer.Length, ReadBytes);
         }
         [TestMethod, TestCategory("ShortTime")]
@@ -82,8 +82,30 @@ namespace Piping.Streams.Tests
             var Time = TimeSpan.FromMilliseconds(10);
             using var stream = new CompletableQueueStream();
             using var TokenSource = CreateTokenSource(Time);
-            Assert.ThrowsException<OperationCanceledException>(
-                () => stream.ReadAsync(buffer.AsMemory(), TokenSource.Token));
+            Assert.ThrowsExceptionAsync<OperationCanceledException>(
+                async() => await stream.ReadAsync(buffer.AsMemory(), TokenSource.Token));
+        }
+        [TestMethod, TestCategory("ShortTime")]
+        public async Task StartReadAsyncTest1()
+        {
+            var data = Enumerable.Range(0, 200).Select(v => (byte)v).ToArray();
+            var buffer = new byte[100];
+            var Time = TimeSpan.FromMilliseconds(100);
+            using var stream = new CompletableQueueStream();
+            await stream.WriteAsync(data);
+            int ReadBytes = 0;
+            using (var TokenSource = new CancellationTokenSource(Time))
+                ReadBytes = await stream.ReadAsync(buffer, TokenSource.Token);
+            Assert.AreEqual(buffer.Length, ReadBytes);
+            ReadBytes = await stream.ReadAsync(buffer);
+            Assert.AreEqual(buffer.Length, ReadBytes);
+            using (var TokenSource = new CancellationTokenSource(Time))
+                await Assert.ThrowsExceptionAsync<OperationCanceledException>(
+                    async () => await stream.ReadAsync(buffer, TokenSource.Token));
+            await stream.WriteAsync(data);
+            Assert.AreEqual(buffer.Length, ReadBytes);
+            ReadBytes = await stream.ReadAsync(buffer);
+            Assert.AreEqual(buffer.Length, ReadBytes);
         }
         [TestMethod, TestCategory("ShortTime")]
         public void StoppedReadAsyncTest2()
@@ -92,8 +114,30 @@ namespace Piping.Streams.Tests
             var Time = TimeSpan.FromMilliseconds(10);
             using var stream = new CompletableQueueStream();
             using var TokenSource = CreateTokenSource(Time);
-            Assert.ThrowsException<OperationCanceledException>(
+            Assert.ThrowsExceptionAsync<OperationCanceledException>(
                 () => stream.ReadAsync(buffer, 0, buffer.Length, TokenSource.Token)) ;
+        }
+        [TestMethod, TestCategory("ShortTime")]
+        public async Task StartReadAsyncTest2()
+        {
+            var data = Enumerable.Range(0, 200).Select(v => (byte)v).ToArray();
+            var buffer = new byte[100];
+            var Time = TimeSpan.FromMilliseconds(100);
+            using var stream = new CompletableQueueStream();
+            await stream.WriteAsync(data, 0, data.Length);
+            int ReadBytes = 0;
+            using (var TokenSource = new CancellationTokenSource(Time))
+                ReadBytes = await stream.ReadAsync(buffer, 0, buffer.Length, TokenSource.Token);
+            Assert.AreEqual(buffer.Length, ReadBytes);
+            ReadBytes = await stream.ReadAsync(buffer, 0, buffer.Length);
+            Assert.AreEqual(buffer.Length, ReadBytes);
+            using (var TokenSource = new CancellationTokenSource(Time))
+                await Assert.ThrowsExceptionAsync<OperationCanceledException>(
+                    () => stream.ReadAsync(buffer, 0, buffer.Length, TokenSource.Token));
+            await stream.WriteAsync(data, 0, data.Length);
+            Assert.AreEqual(buffer.Length, ReadBytes);
+            ReadBytes = await stream.ReadAsync(buffer, 0, buffer.Length);
+            Assert.AreEqual(buffer.Length, ReadBytes);
         }
     }
 }

4c4947f2d06ec2c345e6df1010d991ce73637320
juner <juner@juner.net>
2019-07-02 02:27:58 +0900

    Microsoft.AspNetCore.WwebUtilities を使うのをやめる

--- PipingServer.NET/RequestKey.cs
+++ PipingServer.NET/RequestKey.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using Microsoft.AspNetCore.WebUtilities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Piping
 {
@@ -11,7 +12,7 @@ namespace Piping
         public RequestKey(string relativeUri) : this(new Uri(PATH + relativeUri.TrimStart('/'))) { }
         public RequestKey(Uri relativeUri)
         {
-            var Collection = QueryHelpers.ParseQuery(relativeUri.Query);
+            var Collection = QueryToDictionary(relativeUri.Query);
             Receivers = Collection.TryGetValue("n", out var _n) && int.TryParse(_n, out var __n) ? __n : 1;
             LocalPath = relativeUri.LocalPath.ToLower();
         }
@@ -19,6 +20,23 @@ namespace Piping
         public override bool Equals(object obj)
         {
             return obj is RequestKey other ? other.LocalPath == LocalPath : false;
+        }
+        public static IDictionary<string,string> QueryToDictionary(string Query)
+        {
+            var dic = new Dictionary<string, string>();
+            if (!Query.Any())
+                return dic;
+            if (Query.IndexOf('?') == 0)
+                Query = Query.Substring(1);
+            foreach (var Value in Query?.Split('&') ?? Enumerable.Empty<string>())
+            {
+                var first = Value.IndexOf('=');
+                var hasValue = first >= 0;
+                var k = hasValue ? Value.Substring(0, first) : Value;
+                var v = hasValue ? Value.Substring(first + 1) : string.Empty;
+                dic.Add(k, v);
+            }
+            return dic;
         }
         public override string ToString()
             => $"{LocalPath}?n={Receivers}";

6030a21e947acf508d67cca821877a21b8456ef5
juner <juner@juner.net>
2019-09-04 08:51:56 +0900

    テストの追加途中。

--- PipingServer.NETTests/Controllers/PipingControllerTests.cs
+++ PipingServer.NETTests/Controllers/PipingControllerTests.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PipingServer.NETTests.Controllers
 {
     [TestClass]
     public class PipingControllerTests
     {
-
+        [TestMethod]
+        public void SendAndReceiveTest()
+        {
+            var server = new TestServer(new WebHostBuilder());
+            var client = server.CreateClient();
+        }
     }
 }

aeef52222cdacfba99ae4f1ed6255d85264528c0
juner <juner@juner.net>
2020-07-14 13:15:36 +0900

    Microsoft.AspNetCore.WebUtilities に依存しない実装試み

--- src/PipingServer.Core/Converters/MultipartStreamConverter.cs
+++ src/PipingServer.Core/Converters/MultipartStreamConverter.cs
@@ -2,11 +2,10 @@
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using HttpMultipartParser;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Options;
-using Microsoft.Net.Http.Headers;
-using PipingServer.Core.Internal;
+using PipingServer.Core.Streams;
 using static PipingServer.Core.Properties.Resources;
 
 namespace PipingServer.Core.Converters
@@ -15,20 +14,9 @@ namespace PipingServer.Core.Converters
     {
         readonly MultipartStreamConverterOption Option;
         public MultipartStreamConverter(IOptions<MultipartStreamConverterOption> Options) => Option = Options.Value;
-        // Content-Type: multipart/form-data; boundary="----WebKitFormBoundarymx2fSWqWSd0OxQqq"
-        // The spec says 70 characters is a reasonable limit.
-        string GetBoundary(MediaTypeHeaderValue contentType)
-        {
-            var boundary = HeaderUtilities.RemoveQuotes(contentType.Boundary);
-            if (boundary.IsNullOrWhiteSpace())
-                throw new InvalidDataException(MissingContentTypeBoundary);
-            if (boundary.Length > Option.MultipartBoundaryLengthLimit)
-                throw new InvalidDataException(
-                    string.Format(MultipartBoundaryLengthLimitExceeded, Option.MultipartBoundaryLengthLimit));
-            return boundary.Value;
-        }
         const string MultipartMimeTypeStart = "multipart/";
         const string ContentTypeHeaderName = "Content-Type";
+        const string ContentDispositionHeaderName = "Content-Disposition";
         bool IsMultipartContentType(string contentType)
         {
             return !string.IsNullOrEmpty(contentType)
@@ -36,20 +24,75 @@ namespace PipingServer.Core.Converters
         }
         public bool IsUse(IHeaderDictionary Headers) => IsMultipartContentType((Headers ?? throw new ArgumentNullException(nameof(Headers)))[ContentTypeHeaderName]);
 
-        public async Task<(IHeaderDictionary Headers, Stream Stream)> GetStreamAsync(IHeaderDictionary Headers, Stream Body, CancellationToken Token = default)
+        public Task<(IHeaderDictionary Headers, Stream Stream)> GetStreamAsync(IHeaderDictionary Headers, Stream Body, CancellationToken Token = default)
         {
-            if (Headers == null)
+            if (Headers is null)
                 throw new ArgumentNullException(nameof(Headers));
-            if (Body == null)
+            if (Body is null || Body == System.IO.Stream.Null)
                 throw new ArgumentNullException(nameof(Body));
             if (!Body.CanRead)
                 throw new ArgumentException(NotReadableStream);
-            var ContentType = Headers[ContentTypeHeaderName];
-            var boundary = GetBoundary(MediaTypeHeaderValue.Parse((string)ContentType));
-            var reader = new MultipartReader(boundary, Body, Option.BufferSize);
-            if ((await reader.ReadNextSectionAsync(Token)) is MultipartSection section)
-                return (new HeaderDictionary(section.Headers), section.Body);
-            throw new InvalidOperationException(NoDataStream);
+            var source = new TaskCompletionSource<(IHeaderDictionary Header, Stream Stream)>();
+            var parser = new StreamingMultipartFormDataParser(Body)
+            {
+                BinaryBufferSize = Option.BufferSize,
+            };
+            bool isFirst = true;
+            void ParameterHandler(ParameterPart parameter) { }
+            var Stream = new PipelineStream();
+            void FileHandler(string Name, string FileName, string ContentType, string ContentDisposition, byte[] buffer, int bytes, int partNumber)
+            {
+                if (isFirst && partNumber == 0)
+                {
+                    isFirst = false;
+                    string DispositionString = string.Empty;
+                    if (!string.IsNullOrEmpty(ContentDisposition)) {
+                        DispositionString = ContentDisposition;
+                        if (!string.IsNullOrEmpty(Name))
+                            DispositionString += ";name=" + Name;
+                        if (!string.IsNullOrEmpty(FileName))
+                            DispositionString += ";filename=" + FileName;
+                        Headers[ContentDispositionHeaderName] = DispositionString;
+                    }
+                    if (!string.IsNullOrEmpty(ContentType))
+                    {
+                        Headers[ContentTypeHeaderName] = ContentType;
+                    }
+                    source.TrySetResult((Headers, Stream));
+
+                } else if (partNumber == 0)
+                {
+                    if (!Stream.IsAddingCompleted)
+                        Stream.Complete();
+                }
+                if (Stream.IsAddingCompleted)
+                    return;
+                Stream.Write(buffer, 0, bytes);
+
+            }
+            void StreamCloseHandler()
+            {
+                if (!Stream.IsAddingCompleted)
+                    Stream.Complete();
+                Stream.Dispose();
+            }
+            parser.ParameterHandler += ParameterHandler;
+            parser.FileHandler += FileHandler;
+            parser.StreamClosedHandler += StreamCloseHandler;
+            parser.StreamClosedHandler += () => { };
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    await parser.RunAsync(Token).ConfigureAwait(false);
+                } catch (OperationCanceledException e) {
+                    source.TrySetCanceled(e.CancellationToken);
+                } catch (Exception e)
+                {
+                    source.TrySetException(e);
+                }
+            });
+            return source.Task;
         }
     }
 }
